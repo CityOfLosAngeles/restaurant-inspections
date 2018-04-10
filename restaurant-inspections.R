09c327642c7414ab4694e2dc82124de3a8453264
aescherling <adam.scherling@gmail.com>
2018-04-09 17:30:35 -0700

    now using County open data portal

--- restaurant-inspections.R
+++ restaurant-inspections.R
@@ -9,90 +9,52 @@ library(RSocrata)
 library(dplyr)
 library(magrittr)
 
-# change this to work on the server...
-setwd('~/github/restaurant-inspections')
+# Download the data from the County open data portal
+inspections <- read.socrata("https://data.lacounty.gov/resource/kpth-apsv.json")
+violations <- read.socrata("https://data.lacounty.gov/resource/mfmj-mcsc.json")
 
-# get a list of the files in the directory before the data are downloaded
-existingFiles <- list.files()
+# Filter inspections to just City of LA
+inspections %<>% filter(facility_city=="LOS ANGELES")
 
-# download the zip file which contains inspection data
-zipfileURL <- "http://ehservices.publichealth.lacounty.gov/LIVES/LABusinesses.zip"
-download.file(zipfileURL, destfile="tmp.zip")
+# Sort by date
+inspections %<>% arrange(desc(activity_date))
 
-# unzip the file
-unzip("tmp.zip")
+# Merge violations with inspections
+# Otherwise restaurant name, date, etc. aren't available
+violations %<>% merge(inspections, by="serial_number")
+violations <- violations[!duplicated(violations),]
 
-# read in the files
-businesses <- read.csv('businesses.csv', stringsAsFactors=F)
-feed_info <- read.csv('feed_info.csv', stringsAsFactors=F)
-inspections <- read.csv('inspections.csv', stringsAsFactors=F)
-legend <- read.csv('legend.csv', stringsAsFactors=F)
-violations <- read.csv('violations.csv', stringsAsFactors=F)
+# Sort by date
+violations %<>% arrange(desc(activity_date))
 
-# remove all the files that didn't already exist
-allFiles <- list.files()
-newFiles <- setdiff(allFiles, existingFiles)
-for (file in newFiles) {
-	# change this to work on the server...
-	system(paste0('rm ', file))
+# Download the existing data from the City open data portal
+city_inspections <- read.socrata("https://data.lacity.org/resource/ydjb-vh9c.json")
+
+# If there are no changes, don't upsert
+# I assume that the violation data will only change if the inspection data changes
+upsert <- FALSE
+if (nrow(inspections)!=nrow(city_inspections)) {
+	# if the data have different number of rows, upsert
+	upsert <- TRUE
+} else if (!all.equal(sort(inspections$serial_number), sort(city_inspections$serial_number))) {
+	# if there are any different serial numbers, upsert
+	upsert <- TRUE
 }
 
-## INSPECTIONS
+if (upsert) {
+	# Upload inspections to Socrata
+	write.socrata(dataframe = inspections,
+	              dataset_json_endpoint = "https://data.lacity.org/resource/29fd-3paw.json",
+	              update_mode = "UPSERT",
+	              email = "adam.scherling@lacity.org",
+	              password = user_password)
 
-# fill in the inspection description column using the legend
-for (i in 1:nrow(legend)) {
-	legend_min <- legend$minimum_score[i]
-	legend_max <- legend$maximum_score[i]
-	legend_desc <- legend$description[i]
-	within_range <- (inspections$score >= legend_min) & (inspections$score <= legend_max)
-	inspections$description[within_range] <- legend_desc
+	# Upload violations to Socrata
+	write.socrata(dataframe = violations,
+	              dataset_json_endpoint = "https://data.lacity.org/resource/yf53-6au9.json",
+	              update_mode = "UPSERT",
+	              email = "adam.scherling@lacity.org",
+	              password = user_password)
 }
 
-# merge together businesses and inspections
-business_inspections <- merge(businesses, inspections, by="business_id")
 
-# rename a few variables for clarity
-business_inspections %<>% rename(inspection_date=date,
-								 inspection_score=score,
-								 inspection_description=description,
-								 inspection_type=type)
-
-# filter to City of LA only
-business_inspections %<>% filter(city=="LOS ANGELES")
-
-# sort by date
-business_inspections %<>% arrange(desc(inspection_date))
-
-# read in the Socrata password for posting
-user_password <- readLines("password.txt")
-
-# write the data to Socrata
-write.socrata(dataframe = business_inspections,
-              dataset_json_endpoint = "https://data.lacity.org/resource/yvqx-p6dm.json",
-              update_mode = "REPLACE",
-              email = "adam.scherling@lacity.org",
-              password = user_password)
-
-
-## VIOLATIONS
-
-# merge together businesses and violations
-business_violations <- merge(businesses, violations, by="business_id")
-
-# rename a few variables for clarity
-business_violations %<>% rename(inspection_date=date,
-								violation_code=code,
-								violation_description=description)
-
-# filter to City of LA only
-business_violations %<>% filter(city=="LOS ANGELES")
-
-# sort by date
-business_violations %<>% arrange(desc(inspection_date))
-
-# write the data to Socrata
-write.socrata(dataframe = business_violations,
-              dataset_json_endpoint = "https://data.lacity.org/resource/yf53-6au9.json",
-              update_mode = "REPLACE",
-              email = "adam.scherling@lacity.org",
-              password = user_password)
